75dfecbab359cb7bc39f15097aaa7940c64015b5
JAGS-STI <medina.184757@globalcity.sti.edu.ph>
2023-12-01 19:53:47 +0800

    updated dec 1

---
+++ AdminPage/php/doc.getinfo.php
@@ -0,0 +1,60 @@
+<div class="pageContent">
+        <div class="infosTitle">
+            <h3>Request information</h3>
+            <div class="underline"></div>
+        </div>
+
+        <div class="fields">
+            <div class="fldDetail">
+                <p>Document type:</p>
+                <p id="bold">Barangay Indigency (Educational Support Program)</p>
+            </div>
+            <div class="fldDetail">
+                <p>Name:</p>
+                <p id="bold">Ian Gerald Balbon</p>
+            </div>
+            <div class="fldDetail">
+                <p>VIN/Precint No.:</p>
+                <p id="bold">N/A</p>
+            </div>
+            <div class="fldDetail">
+                <p>No. of years residing in the Barangay:</p>
+                <p id="bold">19 years</p>
+            </div>
+            <div class="fldDetail">
+                <p>Full home address:</p>
+                <p id="bold">Unit 207 MB7 BCDA PDS, Brgy. Ususan, Taguig  City</p>
+            </div>
+            <div class="fldDetail">
+                <p>Purpose:</p>
+                <p id="bold">Scholarship application</p>
+            </div>
+            <div class="fldDetail">
+                <p>Educational/Institution:</p>
+                <p id="bold">STI College Global City</p>
+            </div>
+            <div class="fldDetail">
+                <p>Requesting/support institution:</p>
+                <p id="bold">Commission on Higher Education (CHED)</p>
+            </div>
+        </div>
+
+        <div class="expandsPart">
+            <div class="expands">
+                <p>Set status:</p>
+                <div class="xpnd">
+                    <select id="statusBox" value="Unread" required >
+                        <option value="Pending">Pending</option>
+                        <option value="Ready for pick up">Ready for pick up</option>
+                        <option value="Closed">Closed</option>
+                    </select>
+                </div>
+            </div>
+        </div>
+
+        <div class="buttons">
+            <button>Save</button>
+            <button>Generate PDF</button>
+        </div>
+    
+    </div>
