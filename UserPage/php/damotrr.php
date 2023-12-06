2c474c878a50c69193f1d25b3207507a431c79e6
JAGS-STI <medina.184757@globalcity.sti.edu.ph>
2023-12-06 16:59:08 +0800

    Pdf generation testing

--- UserPage/php/damotrr.php
+++ UserPage/php/damotrr.php
@@ -1,63 +1,63 @@
 <div class="allFields">
-            <div class="field1">
-                <h1 id="headTitle">Request Information</h1>
-                <div class="underline"></div>
+    <div class="field1">
+        <h1 id="headTitle">Request Information</h1>
+        <div class="underline"></div>
 
-                <div class="fldPersonal">
-                    <div class="contactfield">
-                        <p>Name: <span style="color: red">*</span></p>
-                        <div class="inbox">
-                            <input id="nameBox" name="nameBox" type="text" placeholder="Type Here" >
-                        </div>
-                    </div>
-                    <div class="contactfield">
-                        <p>VIN/Precint No.: <span style="color: red">*</span></p>
-                        <div class="inbox">
-                            <input id="contactBox" name="contactBox" type="text" placeholder="Type Here" >
-                        </div>
-                    </div>
-                    <div class="contactfield">
-                        <p>No. of years residing in the Barangay: <span style="color: red">*</span></p>
-                        <div class="inbox">
-                            <input id="contactBox" name="contactBox" type="text" placeholder="Type Here" >
-                        </div>
-                    </div>
+        <div class="fldPersonal">
+            <div class="contactfield">
+                <p>Name: <span style="color: red">*</span></p>
+                <div class="inbox">
+                    <input id="nameBox" name="nameBox" type="text" placeholder="Type Here" >
                 </div>
-
-                <div class="fldPersonal2">
-                    <div class="contactfield">
-                        <p>Full address: <span style="color: red">*</span></p>
-                        <div class="inbox1">
-                            <input id="contactBox" name="contactBox" type="text" placeholder="Type Here" >
-                        </div>
-                    </div>
-                    <div class="contactfield">
-                        <p>Purpose: <span style="color: red">*</span></p>
-                        <div class="inbox">
-                            <input id="contactBox" name="contactBox" type="text" placeholder="Type Here" >
-                        </div>
-                    </div>
+            </div>
+            <div class="contactfield">
+                <p>VIN/Precint No.: <span style="color: red">*</span></p>
+                <div class="inbox">
+                    <input id="contactBox" name="contactBox" type="text" placeholder="Type Here" >
                 </div>
-
-                <div class="fldPersonal3">
-                    <div class="contactfield">
-                        <p>Educational/Institution: <span style="color: red">*</span></p>
-                        <div class="inbox">
-                            <input id="nameBox" name="nameBox" type="text" placeholder="Type Here" >
-                        </div>
-                    </div>
-                    <div class="contactfield">
-                        <p>Requesting/support institution: <span style="color: red">*</span></p>
-                        <div class="inbox">
-                            <input id="contactBox" name="contactBox" type="text" placeholder="Type Here" >
-                        </div>
-                    </div>
+            </div>
+            <div class="contactfield">
+                <p>No. of years residing in the Barangay: <span style="color: red">*</span></p>
+                <div class="inbox">
+                    <input id="contactBox" name="contactBox" type="text" placeholder="Type Here" >
                 </div>
-                
-                <div class="consent">
-                    <p id="consent">I consent to the processing of my personal data in accordance with the Republic Act 10173 or also known as the Data Privacy Act of 2012.</p>
-                    <input id="check" type="checkbox" required/>
-                </div>
-                    <input type="submit" class="submit" name="submit" value="SUBMIT">
             </div>
         </div>
+
+        <div class="fldPersonal2">
+            <div class="contactfield">
+                <p>Full address: <span style="color: red">*</span></p>
+                <div class="inbox1">
+                    <input id="contactBox" name="contactBox" type="text" placeholder="Type Here" >
+                </div>
+            </div>
+            <div class="contactfield">
+                <p>Purpose: <span style="color: red">*</span></p>
+                <div class="inbox">
+                    <input id="contactBox" name="contactBox" type="text" placeholder="Type Here" >
+                </div>
+            </div>
+        </div>
+
+        <div class="fldPersonal3">
+            <div class="contactfield">
+                <p>Educational/Institution: <span style="color: red">*</span></p>
+                <div class="inbox">
+                    <input id="nameBox" name="nameBox" type="text" placeholder="Type Here" >
+                </div>
+            </div>
+            <div class="contactfield">
+                <p>Requesting/support institution: <span style="color: red">*</span></p>
+                <div class="inbox">
+                    <input id="contactBox" name="contactBox" type="text" placeholder="Type Here" >
+                </div>
+            </div>
+        </div>
+        
+        <div class="consent">
+            <p id="consent">I consent to the processing of my personal data in accordance with the Republic Act 10173 or also known as the Data Privacy Act of 2012.</p>
+            <input id="check" type="checkbox" required/>
+        </div>
+            <input type="submit" class="submit" name="submit" value="SUBMIT">
+    </div>
+</div>
