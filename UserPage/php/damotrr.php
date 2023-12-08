7d4c6b9bd2c60faaac826b5431e47fa60531280d
JAGS-STI <medina.184757@globalcity.sti.edu.ph>
2023-12-08 11:33:14 +0800

    size accurate docu added

--- UserPage/php/damotrr.php
+++ UserPage/php/damotrr.php
@@ -1,3 +1,4 @@
+<!-- Barangay Indingency -->
 <div class="allFields">
     <div class="field1">
         <h1 id="headTitle">Request Information</h1>
@@ -61,3 +62,143 @@
             <input type="submit" class="submit" name="submit" value="SUBMIT">
     </div>
 </div>
+
+<!-- Barangay Clearance -->
+<div class="allFields">
+            <div class="field1">
+                <h1 id="headTitle">Request Information</h1>
+                <div class="underline"></div>
+
+                <div class="fldPersonal">
+                    <div class="contactfield">
+                        <p>Name: <span style="color: red">*</span></p>
+                        <div class="inbox">
+                            <input id="nameBox" name="nameBox" type="text" placeholder="Type Here" >
+                        </div>
+                    </div>
+                    <div class="contactfield">
+                        <p>Age: <span style="color: red">*</span></p>
+                        <div class="inbox">
+                            <input id="contactBox" name="contactBox" type="text" placeholder="Type Here" >
+                        </div>
+                    </div>
+                    <div class="contactfield">
+                        <p>Gender: <span style="color: red">*</span></p>
+                        <div class="inbox">
+                            <input id="contactBox" name="contactBox" type="text" placeholder="Type Here" >
+                        </div>
+                    </div>
+
+                    <div class="contactfield">
+                        <p>Birthday: <span style="color: red">*</span></p>
+                        <div class="inbox">
+                            <input id="nameBox" name="nameBox" type="text" placeholder="Type Here" >
+                        </div>
+                    </div>
+                    <div class="contactfield">
+                        <p>Religion: <span style="color: red">*</span></p>
+                        <div class="inbox">
+                            <input id="contactBox" name="contactBox" type="text" placeholder="Type Here" >
+                        </div>
+                    </div>
+                    <div class="contactfield">
+                        <p>Citizenship: <span style="color: red">*</span></p>
+                        <div class="inbox">
+                            <input id="contactBox" name="contactBox" type="text" placeholder="Type Here" >
+                        </div>
+                    </div>
+                </div>
+
+                <div class="fldPersonal2">
+                    <div class="contactfield">
+                        <p>Full home address: <span style="color: red">*</span></p>
+                        <div class="inbox1">
+                            <input id="contactBox" name="contactBox" type="text" placeholder="Type Here" >
+                        </div>
+                    </div>
+                    <div class="contactfield">
+                        <p>Voter’s ID number: <span style="color: red">*</span></p>
+                        <div class="inbox">
+                            <input id="contactBox" name="contactBox" type="text" placeholder="Type Here" >
+                        </div>
+                    </div>
+                </div>
+
+                <div class="fldPersonal3">
+                    <div class="flds">
+                        <div class="contactfield">
+                            <p>Height: <span style="color: red">*</span></p>
+                            <div class="inbox2">
+                                <input id="nameBox" name="nameBox" type="text" placeholder="Type Here" >
+                            </div>
+                        </div>
+                        <div class="contactfield">
+                            <p>Width: <span style="color: red">*</span></p>
+                            <div class="inbox2">
+                                <input id="contactBox" name="contactBox" type="text" placeholder="Type Here" >
+                            </div>
+                        </div>
+                        <div class="contactfield">
+                            <p>Complexion: <span style="color: red">*</span></p>
+                            <div class="inbox2">
+                                <input id="contactBox" name="contactBox" type="text" placeholder="Type Here" >
+                            </div>
+                        </div>
+                    </div>
+                    <div class="contactfield">
+                        <p>Hair color: <span style="color: red">*</span></p>
+                        <div class="inbox">
+                            <input id="contactBox" name="contactBox" type="text" placeholder="Type Here" >
+                        </div>
+                    </div>
+                </div>   
+            </div>
+
+                <div class="field1">
+                    <h1 id="headTitle">Contact Person Information</h1>
+                    <div class="underline"></div>
+                    
+                    <div class="fldPersonal2">
+                        <div class="contactfield">
+                            <p>Contact person: <span style="color: red">*</span></p>
+                            <div class="inbox">
+                                <input id="contactBox" name="contactBox" type="text" placeholder="Type Here" >
+                            </div>
+                        </div>
+                        <div class="contactfield">
+                            <p>Contact number: <span style="color: red">*</span></p>
+                            <div class="inbox">
+                                <input id="contactBox" name="contactBox" type="text" placeholder="Type Here" >
+                            </div>
+                        </div>
+                        <div class="contactfield">
+                            <p>Address: <span style="color: red">*</span></p>
+                            <div class="inbox1">
+                                <input id="contactBox" name="contactBox" type="text" placeholder="Type Here" >
+                        </div>
+                    </div>
+                </div>
+
+                <div class="fldPurpose">
+                        <div class="contactfield">
+                            <p>Purpose of this request: <span style="color: red">*</span></p>
+                            <div class="inbox">
+                                <input id="contactBox" name="contactBox" type="text" placeholder="Type Here" >
+                            </div>
+                        </div>
+                        <div class="contactfield">
+                            <p>Upload your 2x2 photo <span style="color: red">*</span></p>
+                            <button id="upldPic">Upload photo</button>
+                        </div>
+                    </div>
+
+
+
+                
+                <div class="consent">
+                    <p id="consent">I consent to the processing of my personal data in accordance with the Republic Act 10173 or also known as the Data Privacy Act of 2012.</p>
+                    <input id="check" type="checkbox" required/>
+                </div>
+                    <input type="submit" class="submit" name="submit" value="SUBMIT">
+            </div>
+        </div>
